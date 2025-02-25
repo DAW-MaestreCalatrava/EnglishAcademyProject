8325c320cec6115c4e138f3dc3b3c0d59c9b0fd7
Carlos Piña <charlypineapple@gmail.com>
2025-02-25 16:51:07 +0100

    feat: add component icono Sociales (footer)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./components/burguer-component/burguer-component.js";
 import "./components/header-button/header-button-component.js";
+import "./components/iconSocial-component/iconSocial-component.js";
