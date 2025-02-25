ec3d79adf9c93a0cbee892f48094d65ccdcafdd5
javimr11 <javymol11@gmail.com>
2025-02-25 17:18:55 +0100

    Feature/input component (#5)
    * feat:input-component
    
    * fix:input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./components/burguer-component/burguer-component.js";
+import "./components/primary-button/primary-button.js";
+import "./components/input-component/input-component.js";
 import "./components/header-button/header-button-component.js";
 import "./components/texto/texto.js";
 import "./components/banner-component/banner-component.js";
