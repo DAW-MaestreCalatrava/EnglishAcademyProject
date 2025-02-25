1d1d42827a69539e4f9a5a40eea194d8b788cd92
javimr11 <javymol11@gmail.com>
2025-02-25 18:08:23 +0100

    Feature/textarea component (#7)
    * feat:input-component
    
    * fix:input
    
    * feat: add textarea-component
    
    * fix:textarea

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import "./components/input-component/input-component.js";
 import "./components/header-button/header-button-component.js";
 import "./components/texto/texto.js";
 import "./components/banner-component/banner-component.js";
+import "./components/textarea-component/textarea-component.js";
 import "./components/iconSocial-component/iconSocial-component.js";
