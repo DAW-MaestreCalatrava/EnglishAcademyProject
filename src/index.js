a26cc72c2800e7a037c8986dedf8df592a1a8b9f
Carlos Piña <charlypineapple@gmail.com>
2025-02-25 17:41:07 +0100

    feat: actualizar main.js para component icono Sociales (footer)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./components/burguer-component/burguer-component.js";
 import "./components/header-button/header-button-component.js";
 import "./components/texto/texto.js";
 import "./components/banner-component/banner-component.js";
+import "./components/iconSocial-component/iconSocial-component.js";
